de8cccdb35033c850c140c3bbd61bd4bc26bd9f1
Nahue <nahue.santini@gmail.com>
2023-12-09 19:36:18 -0300

    Instalacion de dependencias & Creacion de Banner y NavBAr

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import "./App.css";
+import { Route, Routes } from "react-router";
 
 function App() {
   return (
-    <section class="container">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-      <div>5</div>
-      <div>6</div>
-      <div>7</div>
-    </section>
+    <div>
+      HOLA
+      <Routes>
+        {/* <Route path="/" element={} /> */}
+      </Routes>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+// import store from "./Redux/store";
+// import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+ReactDOM.render(
+  // <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  // </Provider>,
+  document.getElementById("root")
+);
+
