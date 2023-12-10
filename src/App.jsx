455f7ff46873a0b135e5895a58d558022f11281f
Nahue <nahue.santini@gmail.com>
2023-12-09 21:48:40 -0300

    Creacion carpeta views + Portfolio.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Route, Routes } from "react-router";
 import Banner from "./Components/Banner/Banner.jsx";
+import Portfolio from "./views/Portfolio/Portfolio.jsx";
 function App() {
   return (
     <div>
       <Routes>
+        <Route path="/" element={<Portfolio/>}/>
         <Route path="/banner" element={<Banner/>}/>
       </Routes>
     </div>
---
+++ src/views/Portfolio/Portfolio.jsx
@@ -0,0 +1,10 @@
+import Banner from '../../Components/Banner/Banner'
+const Portfolio = () => {
+    return (
+        <>
+        <Banner/>
+        </>
+    )
+}
+
+export default Portfolio
