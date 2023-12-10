f33a40d534f3d2bf591b1daca58aa84cefd1c77a
Nahue <nahue.santini@gmail.com>
2023-12-09 23:16:34 -0300

    Configuraciones

--- src/views/Portfolio/Portfolio.jsx
+++ src/views/Portfolio/Portfolio.jsx
@@ -1,8 +1,10 @@
 import Banner from '../../Components/Banner/Banner'
+import NavBar from '../../Components/NavBar/NavBar'
 const Portfolio = () => {
     return (
         <>
         <Banner/>
+        <NavBar/>
         </>
     )
 }
