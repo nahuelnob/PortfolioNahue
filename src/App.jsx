8f78b3f2cd2f9d8a18324e69fab44923376a7a07
Nahue <nahue.santini@gmail.com>
2023-12-09 21:52:03 -0300

    create Navbar Route + NavBar.module.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { Route, Routes } from "react-router";
 import Banner from "./Components/Banner/Banner.jsx";
 import Portfolio from "./views/Portfolio/Portfolio.jsx";
+import NavBar from "./Components/NavBar/NavBar.jsx";
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Portfolio/>}/>
         <Route path="/banner" element={<Banner/>}/>
+        <Route path="/navbar" element={<NavBar/>}/>
       </Routes>
     </div>
   );
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+import style from './Navbar.module.css'
+const NavBar = () => {
+    return (
+        <nav className={style.container}>
+
+        </nav>
+    )
+}
+
+export default NavBar
