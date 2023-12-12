a388a3443de3525ce8b5496b00cfd1cfeaa0c205
Nahue <nahue.santini@gmail.com>
2023-12-12 20:17:29 -0300

    Cambios en Navbar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,62 +2,72 @@ import { useEffect, useState } from "react";
 import style from "./Navbar.module.css";
 const NavBar = () => {
   const [desplegado, SetDesplegado] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const handlerDesplegar = () => {
     desplegado ? SetDesplegado(false) : SetDesplegado(true);
   };
 
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []); // El segundo argumento vacío asegura que este efecto solo se ejecute una vez al montar el componente
+
   return (
     <nav className={desplegado ? style.containerDesplegado : style.container}>
-      <button className={style.desplegar} onClick={handlerDesplegar}>
+      <img
+        src="https://res.cloudinary.com/nahuesantini/image/upload/v1702420610/brand/isologoBlanco_lrs5ma.png"
+        alt=""
+        className={style.logo}
+      />
+      <button className={style.botonDesplegar} onClick={handlerDesplegar}>
         <span class="material-symbols-outlined">add_circle</span>
       </button>
-      {desplegado ? (
-        <div className={style.links}>Sobre Mi</div>
-      ) : (
-        <div class="material-symbols-outlined">person</div>
-      )}
-      <div className={style.separador}>|</div>
-      {desplegado ? (
-        <div className={style.links}>Proyectos</div>
-      ) : (
-        <div class="material-symbols-outlined">terminal</div>
-      )}
+      <div className={style.espacio}></div>
+      <div className={style.espacio}></div>
+      <div className={style.espacio}></div>
+      <div className={style.espacio}></div>
+      <div className={style.espacio}></div>
+      <div className={style.espacio}></div>
+      <div className={style.espacio}></div>
+      <div>
+        {desplegado || windowWidth > 768 ? (
+          <div className={style.links}>Sobre Mi</div>
+        ) : (
+          <div class="material-symbols-outlined">person</div>
+        )}
+      </div>
+      {/* <div className={style.separador}>|</div> */}
+      <div>
+        {desplegado || windowWidth > 768 ? (
+          <div className={style.links}>Proyectos</div>
+        ) : (
+          <div class="material-symbols-outlined">terminal</div>
+        )}
+      </div>
 
-      <div className={style.separador}>|</div>
-      {desplegado ? (
-        <div className={style.links}>Contacto</div>
-      ) : (
-        <div class="material-symbols-outlined">contacts</div>
-      )}
-      <div className={style.separador}>|</div>
-      {desplegado ? (
-        <div className={style.links}>Modo</div>
-      ) : (
-        <div class="material-symbols-outlined">contacts</div>
-      )}
-
-      {/* <img
-        src="https://res.cloudinary.com/nahuesantini/image/upload/v1702163789/brand/isologoBlanco_r3uhb5.png"
-        alt=""
-        className={style.logo}
-      />
-
-      <img
-        src="https://res.cloudinary.com/nahuesantini/image/upload/v1702161475/brand/logoBlaco_sqtghg.png"
-        alt=""
-        className={style.logo}
-      />
-      <img
-        src="https://res.cloudinary.com/nahuesantini/image/upload/v1702161474/brand/isologo_q5fmwz.png"
-        alt=""
-        className={style.logo}
-      />
-      <img
-        src="https://res.cloudinary.com/nahuesantini/image/upload/v1702418333/brand/isologoBlanco2_kalaaz.png"
-        alt=""
-        className={style.logo}
-      /> */}
-
+      {/* <div className={style.separador}>|</div> */}
+      <div>
+        {desplegado || windowWidth > 768 ? (
+          <div className={style.links}>Contacto</div>
+        ) : (
+          <div class="material-symbols-outlined">contacts</div>
+        )}
+      </div>
+      <div>
+        {desplegado || windowWidth > 768 ? (
+          <div className={style.links}>Modo</div>
+        ) : (
+          <div class="material-symbols-outlined">contacts</div>
+        )}
+        {/* <div className={style.separador}>|</div> */}
+      </div>
     </nav>
   );
 };
