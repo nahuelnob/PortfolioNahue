b87e7d13648f1f5a0509f0cdd091267daa66ab63
Nahue <nahue.santini@gmail.com>
2023-12-15 18:15:25 -0300

    About cambios

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -51,16 +51,16 @@ const About = () => {
             src="https://img.icons8.com/color/240/javascript--v1.png"
             alt="javascript--v1"
           />
-            <img
-              className={style.skill}
-              src="https://img.icons8.com/color/240/css3.png"
-              alt="css3"
-            />
-            <img
-              className={style.skill}
-              src="https://img.icons8.com/color/240/html-5--v1.png"
-              alt="html-5--v1"
-            />
+          <img
+            className={style.skill}
+            src="https://img.icons8.com/color/240/css3.png"
+            alt="css3"
+          />
+          <img
+            className={style.skill}
+            src="https://img.icons8.com/color/240/html-5--v1.png"
+            alt="html-5--v1"
+          />
           <img
             className={style.skill}
             src="https://img.icons8.com/color/240/python--v1.png"
@@ -86,11 +86,11 @@ const About = () => {
             src="https://img.icons8.com/color/240/tailwind_css.png"
             alt="tailwind_css"
           />
-            <img
-              className={style.skill}
-              src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/96/external-postgre-sql-a-free-and-open-source-relational-database-management-system-logo-color-tal-revivo.png"
-              alt="external-postgre-sql-a-free-and-open-source-relational-database-management-system-logo-color-tal-revivo"
-            />
+          <img
+            className={style.skill}
+            src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/96/external-postgre-sql-a-free-and-open-source-relational-database-management-system-logo-color-tal-revivo.png"
+            alt="external-postgre-sql-a-free-and-open-source-relational-database-management-system-logo-color-tal-revivo"
+          />
           <img
             className={style.skill}
             src="https://res.cloudinary.com/nahuesantini/image/upload/v1702607522/Portfolio/sequelize_c60cud.png"
@@ -107,32 +107,33 @@ const About = () => {
             alt="github"
           />
         </section>
-
-        {/* <img
-          src="https://res.cloudinary.com/nahuesantini/image/upload/v1698707283/Portfolio/CoderHouse_zmnyhd.png"
-          alt=""
-          width={"40px"}
-        />
-        <img
-          src="https://res.cloudinary.com/nahuesantini/image/upload/v1698708352/Portfolio/572ceff3-3da7-4086-9ac0-5028ad78c930_d7vcby.jpg"
-          alt=""
-          width={"40px"}
-        />
-        <img
-          src="https://res.cloudinary.com/nahuesantini/image/upload/v1698708342/Portfolio/a5b3af8c-3457-4d41-9380-1d33824f8711_jhjphs.jpg"
-          alt=""
-          width={"40px"}
-        />
-        <img
-          src="https://res.cloudinary.com/nahuesantini/image/upload/v1702420270/Portfolio/argentina_programa_tramo3_qp7i9k.jpg"
-          alt=""
-          width={"40px"}
-        />
-        <img
-          src="https://res.cloudinary.com/nahuesantini/image/upload/v1702420093/Portfolio/HenryCertificado_ln4qib.jpg"
-          alt=""
-          width={"40px"}
-        /> */}
+        {/* <section>
+          <img
+            src="https://res.cloudinary.com/nahuesantini/image/upload/v1698707283/Portfolio/CoderHouse_zmnyhd.png"
+            alt=""
+            width={"40px"}
+          />
+          <img
+            src="https://res.cloudinary.com/nahuesantini/image/upload/v1698708352/Portfolio/572ceff3-3da7-4086-9ac0-5028ad78c930_d7vcby.jpg"
+            alt=""
+            width={"40px"}
+          />
+          <img
+            src="https://res.cloudinary.com/nahuesantini/image/upload/v1698708342/Portfolio/a5b3af8c-3457-4d41-9380-1d33824f8711_jhjphs.jpg"
+            alt=""
+            width={"40px"}
+          />
+          <img
+            src="https://res.cloudinary.com/nahuesantini/image/upload/v1702420270/Portfolio/argentina_programa_tramo3_qp7i9k.jpg"
+            alt=""
+            width={"40px"}
+          />
+          <img
+            src="https://res.cloudinary.com/nahuesantini/image/upload/v1702420093/Portfolio/HenryCertificado_ln4qib.jpg"
+            alt=""
+            width={"40px"}
+          />
+        </section> */}
       </div>
     </section>
   );
