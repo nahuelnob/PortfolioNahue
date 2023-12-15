7dd2b1e99ac12187ef7b4846f123e3d057b4c7af
Nahue <nahue.santini@gmail.com>
2023-12-15 00:30:22 -0300

    cambios en about

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -13,19 +13,101 @@ const About = () => {
         <section>
           <img
             className={style.persona}
-            src="https://res.cloudinary.com/nahuesantini/image/upload/v1698707253/Portfolio/IMG_20220813_110728364_PORTRAIT_wesmz0.png"
+            // src="https://res.cloudinary.com/nahuesantini/image/upload/v1698707253/Portfolio/IMG_20220813_110728364_PORTRAIT_wesmz0.png"
+            src="https://res.cloudinary.com/nahuesantini/image/upload/v1702605340/brand/nahue_fndepj.png"
             alt=""
           />
           <img
             className={style.foto}
-            src="https://res.cloudinary.com/nahuesantini/image/upload/v1698707247/Portfolio/NahueParque_kitgwp.jpg"
+            // src="https://res.cloudinary.com/nahuesantini/image/upload/v1698707247/Portfolio/NahueParque_kitgwp.jpg"
+            src="https://res.cloudinary.com/nahuesantini/image/upload/v1702605341/brand/NahueParque_uc1iee.jpg"
             alt=""
           />
         </section>
       </div>
 
       <div className={style.column2}>
-        <img
+      <div className={style.presentacion}>
+        <p>
+          Mi búsqueda constante de crecimiento personal y profesional me ha
+          llevado a enfrentar desafíos en el emocionante mundo de la tecnología.
+        </p>
+        <p>
+          Tanto el desarrollo de aplicaciones y sitios web, como la exploración
+          de datos, me han inspirado y me han permitido combinar mis habilidades
+          y pasiones.
+        </p>
+        <p>
+          Me he formado en campos tan diversos como
+          Economía y Diseño Gráfico. Estoy emocionado por seguir desarrollándome
+          profesionalmente y abierto a nuevas oportunidades que me permitan
+          aplicar mis conocimientos y habilidades.
+        </p>
+      </div>
+      <section className={style.skills}>
+          <img
+            className={style.skill}
+            src="https://img.icons8.com/color/240/javascript--v1.png"
+            alt="javascript--v1"
+          />
+            <img
+              className={style.skill}
+              src="https://img.icons8.com/color/240/css3.png"
+              alt="css3"
+            />
+            <img
+              className={style.skill}
+              src="https://img.icons8.com/color/240/html-5--v1.png"
+              alt="html-5--v1"
+            />
+          <img
+            className={style.skill}
+            src="https://img.icons8.com/color/240/python--v1.png"
+            alt="python--v1"
+          />
+          <img
+            className={style.skill}
+            src="https://img.icons8.com/color/240/react-native.png"
+            alt="react-native"
+          />
+          <img
+            className={style.skill}
+            src="https://img.icons8.com/color/240/redux.png"
+            alt="redux"
+          />
+          <img
+            className={style.skill}
+            src="https://img.icons8.com/color/240/nodejs.png"
+            alt="nodejs"
+          />
+          <img
+            className={style.skill}
+            src="https://img.icons8.com/color/240/tailwind_css.png"
+            alt="tailwind_css"
+          />
+            <img
+              className={style.skill}
+              src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/96/external-postgre-sql-a-free-and-open-source-relational-database-management-system-logo-color-tal-revivo.png"
+              alt="external-postgre-sql-a-free-and-open-source-relational-database-management-system-logo-color-tal-revivo"
+            />
+          <img
+            className={style.skill}
+            src="https://res.cloudinary.com/nahuesantini/image/upload/v1702607522/Portfolio/sequelize_c60cud.png"
+            alt="tailwind_css"
+          />
+          <img
+            className={style.skill}
+            src="https://img.icons8.com/color/240/git.png"
+            alt="git"
+          />
+          <img
+            className={style.skill}
+            src="https://img.icons8.com/fluency/240/github.png"
+            alt="github"
+          />
+        </section>
+
+        {/* <img
           src="https://res.cloudinary.com/nahuesantini/image/upload/v1698707283/Portfolio/CoderHouse_zmnyhd.png"
           alt=""
           width={"40px"}
@@ -49,28 +131,10 @@ const About = () => {
           src="https://res.cloudinary.com/nahuesantini/image/upload/v1702420093/Portfolio/HenryCertificado_ln4qib.jpg"
           alt=""
           width={"40px"}
-        />
-        <i class="fa-brands fa-node"></i>
-        <i class="fa-brands fa-js"></i>
-        <i class="fa-brands fa-python"></i>
-        <i class="fa-brands fa-react"></i>
-        <i class="fa-brands fa-css3-alt"></i>
-        <i class="fa-brands fa-html5"></i>
+        /> */}
+        
       </div>
-      <div className={style.presentacion}>
-          <p>
-            Soy Nahuel, desarrollador Full Stack Jr y Analista Data Science jr.
-            Mi búsqueda constante de crecimiento personal y profesional me ha
-            llevado a enfrentar desafíos en el emocionante mundo de la
-            tecnología. Tanto el desarrollo de aplicaciones y sitios web, como
-            la exploración de datos, me han inspirado y me han permitido
-            combinar mis habilidades y pasiones. A lo largo de mi trayecto, he
-            estudiado campos tan diversos como Economía y Diseño Gráfico. Estoy
-            emocionado por seguir desarrollándome profesionalmente y abierto a
-            nuevas oportunidades que me permitan aplicar mis conocimientos y
-            habilidades.
-          </p>
-        </div>
+
     </section>
   );
 };
