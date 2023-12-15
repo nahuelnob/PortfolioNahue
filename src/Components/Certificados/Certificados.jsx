a7cf6e58a0afce71522f104346c50d290efe1b14
Nahue <nahue.santini@gmail.com>
2023-12-15 18:18:46 -0300

    Creacion Certificados

---
+++ src/Components/Certificados/Certificados.jsx
@@ -0,0 +1,36 @@
+import style from "./Certificados.module.css";
+const Certificados = () => {
+  return (
+    <div className={style.container}>
+      <section>
+        <img
+          src="https://res.cloudinary.com/nahuesantini/image/upload/v1698707283/Portfolio/CoderHouse_zmnyhd.png"
+          alt=""
+          width={"40px"}
+        />
+        <img
+          src="https://res.cloudinary.com/nahuesantini/image/upload/v1698708352/Portfolio/572ceff3-3da7-4086-9ac0-5028ad78c930_d7vcby.jpg"
+          alt=""
+          width={"40px"}
+        />
+        <img
+          src="https://res.cloudinary.com/nahuesantini/image/upload/v1698708342/Portfolio/a5b3af8c-3457-4d41-9380-1d33824f8711_jhjphs.jpg"
+          alt=""
+          width={"40px"}
+        />
+        <img
+          src="https://res.cloudinary.com/nahuesantini/image/upload/v1702420270/Portfolio/argentina_programa_tramo3_qp7i9k.jpg"
+          alt=""
+          width={"40px"}
+        />
+        <img
+          src="https://res.cloudinary.com/nahuesantini/image/upload/v1702420093/Portfolio/HenryCertificado_ln4qib.jpg"
+          alt=""
+          width={"40px"}
+        />
+      </section>
+    </div>
+  );
+};
+
+export default Certificados
