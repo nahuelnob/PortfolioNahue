670324488e75d33d7a5333aa53ec992147d30a0f
Nahue <nahue.santini@gmail.com>
2023-12-15 18:43:49 -0300

    certificados

--- src/Components/Certificados/Certificados.jsx
+++ src/Components/Certificados/Certificados.jsx
@@ -2,35 +2,51 @@ import style from "./Certificados.module.css";
 const Certificados = () => {
   return (
     <div className={style.container}>
-      <section>
-        <img
-          src="https://res.cloudinary.com/nahuesantini/image/upload/v1698707283/Portfolio/CoderHouse_zmnyhd.png"
-          alt=""
-          width={"40px"}
-        />
-        <img
-          src="https://res.cloudinary.com/nahuesantini/image/upload/v1698708352/Portfolio/572ceff3-3da7-4086-9ac0-5028ad78c930_d7vcby.jpg"
-          alt=""
-          width={"40px"}
-        />
-        <img
-          src="https://res.cloudinary.com/nahuesantini/image/upload/v1698708342/Portfolio/a5b3af8c-3457-4d41-9380-1d33824f8711_jhjphs.jpg"
-          alt=""
-          width={"40px"}
-        />
-        <img
-          src="https://res.cloudinary.com/nahuesantini/image/upload/v1702420270/Portfolio/argentina_programa_tramo3_qp7i9k.jpg"
-          alt=""
-          width={"40px"}
-        />
-        <img
-          src="https://res.cloudinary.com/nahuesantini/image/upload/v1702420093/Portfolio/HenryCertificado_ln4qib.jpg"
-          alt=""
-          width={"40px"}
-        />
+      <section className={style.carousel}>
+        <div className={style.henry}>.</div>
+        <div className={style.tramo1}>.</div>
+        <div className={style.tramo2}>.</div>
+        <div className={style.tramo3}>.</div>
+        <div className={style.coder}>.</div>
+
+        {/* <div className={style.containerCertificado}>
+          <img
+            className={style.certificado}
+            src="https://res.cloudinary.com/nahuesantini/image/upload/v1702420093/Portfolio/HenryCertificado_ln4qib.jpg"
+            alt="Henry"
+          />
+        </div>
+        <div className={style.containerCertificado}>
+          <img
+            className={style.certificado}
+            src="https://res.cloudinary.com/nahuesantini/image/upload/v1698708352/Portfolio/572ceff3-3da7-4086-9ac0-5028ad78c930_d7vcby.jpg"
+            alt="1er Tramo"
+          />
+        </div>
+        <div className={style.containerCertificado}>
+          <img
+            className={style.certificado}
+            src="https://res.cloudinary.com/nahuesantini/image/upload/v1698708342/Portfolio/a5b3af8c-3457-4d41-9380-1d33824f8711_jhjphs.jpg"
+            alt="2do Tramo"
+          />
+        </div>
+        <div className={style.containerCertificado}>
+          <img
+            className={style.certificado}
+            src="https://res.cloudinary.com/nahuesantini/image/upload/v1702420270/Portfolio/argentina_programa_tramo3_qp7i9k.jpg"
+            alt="3er Tramo"
+          />
+        </div>
+        <div className={style.containerCertificado}>
+          <img
+            className={style.certificado}
+            src="https://res.cloudinary.com/nahuesantini/image/upload/v1698707283/Portfolio/CoderHouse_zmnyhd.png"
+            alt="CoderHouse"
+          />
+        </div> */}
       </section>
     </div>
   );
 };
 
-export default Certificados
+export default Certificados;
