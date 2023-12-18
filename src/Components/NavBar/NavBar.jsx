86b52b3a369bf253e444fb195e36e24329ef2008
Nahue <nahue.santini@gmail.com>
2023-12-18 19:58:58 -0300

    Cambios en la NavBAr

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -8,27 +8,53 @@ const NavBar = () => {
         alt=""
         className={style.logo}
       />
+      <hr style={{position:"absolute", width:"3rem", top:"5rem"}}/>
 
       <div className={style.links}>
         <div className={style.link}>
-          <p>Sobre Mi</p>
+          <i class="fa-solid fa-user"></i>
         </div>
         <div className={style.link}>
-          <p>Proyectos</p>
+          <i class="fa-solid fa-briefcase"></i>
         </div>
         <div className={style.link}>
-          <p>Contacto</p>
+          <i class="fa-solid fa-address-card" />
         </div>
-        <div className={style.toggle}><Toggle/></div>
-      </div>
-
-      <div className={style.linksDesp}>
-        <i class="fa-solid fa-user"></i>
-        <i class="fa-solid fa-briefcase"></i>
-        <i class="fa-solid fa-address-card" />
+        <div className={style.toggle}>
+          {/* <Toggle /> */}
+        </div>
       </div>
     </nav>
   );
 };
 
 export default NavBar;
+
+{
+  /* <nav className={style.container}>
+<img
+  src="https://res.cloudinary.com/nahuesantini/image/upload/v1702420610/brand/isologoBlanco_lrs5ma.png"
+  alt=""
+  className={style.logo}
+  />
+
+  <div className={style.links}>
+  <div className={style.link}>
+    <p>Sobre Mi</p>
+    </div>
+    <div className={style.link}>
+    <p>Proyectos</p>
+  </div>
+  <div className={style.link}>
+  <p>Contacto</p>
+  </div>
+  <div className={style.toggle}><Toggle/></div>
+  </div>
+  
+  <div className={style.linksDesp}>
+  <i class="fa-solid fa-user"></i>
+  <i class="fa-solid fa-briefcase"></i>
+  <i class="fa-solid fa-address-card" />
+</div> 
+</nav>*/
+}
