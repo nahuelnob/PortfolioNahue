571942299c5fd45568e5ca973e75f8822b29c1f1
Nahue <nahue.santini@gmail.com>
2023-12-20 17:16:50 -0300

    Nuevo componente en views

--- src/views/Portfolio/Portfolio.jsx
+++ src/views/Portfolio/Portfolio.jsx
@@ -2,12 +2,14 @@ import About from '../../Components/About/About'
 import Banner from '../../Components/Banner/Banner'
 import Certificados from '../../Components/Certificados/Certificados'
 import NavBar from '../../Components/NavBar/NavBar'
+import Proyectos from '../../Components/Proyectos/Proyectos'
 const Portfolio = () => {
     return (
         <>
         {/* <Banner/> */}
         <NavBar/>
         <About/>
+        <Proyectos/>
         <Certificados/>
         </>
     )
