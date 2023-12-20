274fb2326fdcba9479e8c3649a7a1986623eba8a
Nahue <nahue.santini@gmail.com>
2023-12-19 23:35:11 -0300

    Construccion de Carrusel

--- src/Components/Certificados/Certificados.jsx
+++ src/Components/Certificados/Certificados.jsx
@@ -1,51 +1,79 @@
+import { useState } from "react";
 import style from "./Certificados.module.css";
 const Certificados = () => {
+  const [activo, setActivo] = useState({
+    primero: true,
+    segundo: false,
+    tercero: false,
+    cuarto: false,
+    quinto: false,
+  });
+
+  const handlerActivar = (e) => {
+    const botonPresionado = e.target.value;
+
+    if (!activo[botonPresionado]) {
+      setActivo((prevActivo) => ({
+        [botonPresionado]: !prevActivo[botonPresionado],
+      }));
+    }
+  };
+
   return (
     <div className={style.container}>
-      <i class="fi fi-rs-id-badge">nahue</i>
-      <i class="fi fi-sr-home"></i>
-      <section className={style.carousel}>
-        <div className={style.henry}>.</div>
-        <div className={style.tramo1}>.</div>
-        <div className={style.tramo2}>.</div>
-        <div className={style.tramo3}>.</div>
-        <div className={style.coder}>.</div>
-
-        {/* <div className={style.containerCertificado}>
-          <img
-            className={style.certificado}
-            src="https://res.cloudinary.com/nahuesantini/image/upload/v1702420093/Portfolio/HenryCertificado_ln4qib.jpg"
-            alt="Henry"
-          />
-        </div>
-        <div className={style.containerCertificado}>
-          <img
-            className={style.certificado}
-            src="https://res.cloudinary.com/nahuesantini/image/upload/v1698708352/Portfolio/572ceff3-3da7-4086-9ac0-5028ad78c930_d7vcby.jpg"
-            alt="1er Tramo"
-          />
-        </div>
-        <div className={style.containerCertificado}>
-          <img
-            className={style.certificado}
-            src="https://res.cloudinary.com/nahuesantini/image/upload/v1698708342/Portfolio/a5b3af8c-3457-4d41-9380-1d33824f8711_jhjphs.jpg"
-            alt="2do Tramo"
-          />
-        </div>
-        <div className={style.containerCertificado}>
-          <img
-            className={style.certificado}
-            src="https://res.cloudinary.com/nahuesantini/image/upload/v1702420270/Portfolio/argentina_programa_tramo3_qp7i9k.jpg"
-            alt="3er Tramo"
-          />
-        </div>
-        <div className={style.containerCertificado}>
-          <img
-            className={style.certificado}
-            src="https://res.cloudinary.com/nahuesantini/image/upload/v1698707283/Portfolio/CoderHouse_zmnyhd.png"
-            alt="CoderHouse"
-          />
-        </div> */}
+      <main className={style.carousel}>
+        {activo.primero ? (
+          <div className={style.henry}></div>
+        ) : (
+          <div className={style.henryD}></div>
+        )}
+        {activo.segundo ? (
+          <div className={style.tramo1}></div>
+        ) : (
+          <div className={style.tramo1D}></div>
+        )}
+        {activo.tercero ? (
+          <div className={style.tramo2}></div>
+        ) : (
+          <div className={style.tramo2D}></div>
+        )}
+        {activo.cuarto ? (
+          <div className={style.tramo3}></div>
+        ) : (
+          <div className={style.tramo3D}></div>
+        )}
+        {activo.quinto ? (
+          <div className={style.coder}></div>
+        ) : (
+          <div className={style.coderD}></div>
+        )}
+      </main>
+      <section className={style.botonera}>
+        <button
+          onClick={handlerActivar}
+          value="primero"
+          className={style.boton}
+        ></button>
+        <button
+          onClick={handlerActivar}
+          value="segundo"
+          className={style.boton}
+        ></button>
+        <button
+          onClick={handlerActivar}
+          value="tercero"
+          className={style.boton}
+        ></button>
+        <button
+          onClick={handlerActivar}
+          value="cuarto"
+          className={style.boton}
+        ></button>
+        <button
+          onClick={handlerActivar}
+          value="quinto"
+          className={style.boton}
+        ></button>
       </section>
     </div>
   );
