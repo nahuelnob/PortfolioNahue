568d176e036f4a45a8793a0fa1a2a4dd5b90ad21
Nahue <nahue.santini@gmail.com>
2024-01-08 17:10:40 -0300

    actualizacion

--- src/Components/Proyectos/Proyectos.jsx
+++ src/Components/Proyectos/Proyectos.jsx
@@ -6,10 +6,19 @@ const Proyectos = () => {
         <h1 className={style.titulo}>Proyectos</h1>
       </header>
       <main className={style.main}>
-        <div className={style.countries}>
-          <section className={style.card}></section>
+        <section className={style.countries}>
+          <div className={style.imgCountries}></div>
+        <hr />
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, corrupti! Veritatis, beatae? Corrupti voluptatum quia porro beatae sit, non eos saepe temporibus quasi ab rem voluptates voluptate reprehenderit assumenda doloremque!</p>
+        </section>
+        <section className={style.codecraft}>
+          <div className={style.imgCodecraft}></div>
+
+        </section>
+        <div className={style.padi}>
+          <section className={style.imgPadi}></section>
+
         </div>
-        <div className={style.codecraft}></div>
       </main>
     </div>
   );
