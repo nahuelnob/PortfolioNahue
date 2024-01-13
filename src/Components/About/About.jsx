8dbe026747175ee3493d4453b2e1010e97c3a517
Nahue <nahue.santini@gmail.com>
2024-01-13 20:40:03 -0300

    about fin

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -7,39 +7,39 @@ const About = () => {
         <section className={style.column1}>
           <img
             className={style.persona}
-            // src="https://res.cloudinary.com/nahuesantini/image/upload/v1698707253/Portfolio/IMG_20220813_110728364_PORTRAIT_wesmz0.png"
+
             src="https://res.cloudinary.com/nahuesantini/image/upload/v1702605340/brand/nahue_fndepj.png"
-            // src="https://res.cloudinary.com/nahuesantini/image/upload/v1702669918/brand/euhan_uygofm.png"
+
             alt=""
           />
+          <article className={style.puesto}>
+            <h1 className={style.fs}>FullStack</h1>
+            <h1 className={style.dev}>developer</h1>
+            <h1 className={style.ds}>Data SCience</h1>
+            <h1 className={style.jr}>Analyst jr.</h1>
+          </article>
         </section>
         <section className={style.column2}>
           <h1 className={style.nombre}>NAHUEL</h1>
           <h1 className={style.apellido}>SANTINI</h1>
-          <hr className={style.hr} />
-          <h1 className={style.fs}>FullStack<span className={style.dev}>developer</span></h1>
+          <div className={style.hr} />
 
-          {/* <h1 className={style.ds}>Data Science</h1> */}
-          {/* <h1 className={style.jr}>Analyst jr.</h1> */}
-          {/* <h1 className={style.puesto}>
-          FullStack Developer <span className={style.anpersand}>&&</span> Data
-          Science Analyst Jr.
-        </h1> */}
-          {/* <article className={style.article}>
+
+          <article className={style.article}>
             <p>
-              ¡Hola! Soy Nahuel Santini, desarrollador full stack y analista de
-              data science jr. Mi experiencia abarca desde ventas, pasa por
+              ¡Hola! Soy Nahuel Santini, desarrollador fullstack y analista de
+              data science jr. Mi experiencia abarca desde ventas,
               diseño gráfico hasta economía. Con habilidades versátiles y un
-              enfoque integral, disfruto creando soluciones impactantes. */}
-          {/* <p>
-              {" "}
-              Desde el frontend hasta el análisis de datos, estoy listo para
-              contribuir al éxito de tu equipo. ¡Aca te muestro algunos de mis
-              proyectos!
-            </p> */}
-          {/* </p>
-          </article> */}
-          {/* <section className={style.skills}>
+              enfoque integral, disfruto creando soluciones impactantes.
+              <p>
+                {" "}
+                Desde el frontend hasta el análisis de datos, estoy listo para
+                contribuir al éxito de tu equipo. ¡A continuación te muestro algunos de mis
+                proyectos!
+              </p>
+            </p>
+          </article>
+          <section className={style.skills}>
             <img
               className={style.skill}
               src="https://img.icons8.com/color/240/javascript--v1.png"
@@ -100,8 +100,8 @@ const About = () => {
               src="https://img.icons8.com/fluency/240/github.png"
               alt="github"
             />
-          </section> */}
-          {/* <hr className={style.hr} /> */}
+          </section>
+          <div className={style.hrVert} />
         </section>
       </main>
 
