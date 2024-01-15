edd5df1f3300d6890dd695d6e89f6a589ed28738
Nahue <nahue.santini@gmail.com>
2024-01-15 17:48:07 -0300

    cambios en proyectos

--- src/Components/Proyectos/Proyectos.jsx
+++ src/Components/Proyectos/Proyectos.jsx
@@ -2,21 +2,30 @@ import style from "./Proyectos.module.css";
 const Proyectos = () => {
   return (
     <div className={style.container}>
+      <div className={style.separador}></div>
       <header className={style.header}>
         <h1 className={style.titulo}>Proyectos</h1>
       </header>
+
       <main className={style.main}>
         <section className={style.countries}>
           <div className={style.imgCountries}></div>
-        <hr />
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, corrupti! Veritatis, beatae? Corrupti voluptatum quia porro beatae sit, non eos saepe temporibus quasi ab rem voluptates voluptate reprehenderit assumenda doloremque!</p>
+          <aside>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, corrupti! Veritatis, beatae? Corrupti voluptatum quia porro beatae sit, non eos saepe temporibus quasi ab rem voluptates voluptate reprehenderit assumenda doloremque!</p>
+          </aside>
         </section>
         <section className={style.codecraft}>
+          <aside>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, corrupti! Veritatis, beatae? Corrupti voluptatum quia porro beatae sit, non eos saepe temporibus quasi ab rem voluptates voluptate reprehenderit assumenda doloremque!</p>
+          </aside>
           <div className={style.imgCodecraft}></div>
 
         </section>
         <div className={style.padi}>
           <section className={style.imgPadi}></section>
+          <aside>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, corrupti! Veritatis, beatae? Corrupti voluptatum quia porro beatae sit, non eos saepe temporibus quasi ab rem voluptates voluptate reprehenderit assumenda doloremque!</p>
+          </aside>
 
         </div>
       </main>
