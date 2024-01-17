92bcb9cd57230034c39afabe66a94151279c7bb4
Nahue <nahue.santini@gmail.com>
2024-01-17 00:22:26 -0300

    YouTube + galeria

--- src/Components/Proyectos/Proyectos.jsx
+++ src/Components/Proyectos/Proyectos.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import style from "./Proyectos.module.css";
 
 const Proyectos = () => {
-  ////////////////////////////////////////////////////
 
+  ////////////////////////////////////////////////////
+  const [galeria, setGaleria] = useState(0)
+
+  const handlerChange = (value) => {
+    galeria > 3 ? setGaleria(0) : setGaleria(galeria + value)
+    galeria < 1 ? setGaleria(4) : setGaleria(galeria + value)
+  }
+  console.log(galeria);
+  ////////////////////////////////////////////////////
+  const [youTube, setYoutube] = useState(false)
+
+  const handler = () => {
+    setYoutube(!youTube)
+  }
 
   // Coloca aquí la URL del video de YouTube que deseas mostrar
-  const videoUrl = 'https://www.youtube.com/watch?v=vE5zUr5URqY';
+  const videoUrl = 'https://www.youtube.com/watch?v=C7ZdBGwuOuQ';
 
   // Extraer el código del video desde la URL
   const videoCode = videoUrl.split('v=')[1];
@@ -18,25 +32,39 @@ const Proyectos = () => {
       </header>
 
       <main className={style.main}>
-        {/* <div>
+        <section className={style.countries}>
+          <div className={style.imgCountries}></div>
+          <aside className={style.texto} style={{ display: "flex", flexDirection: "column" }}>
+            <h1 className={style.subtitulo}>Countries App</h1>
+            <div className={style.separadorTexto}></div>
+            <p>App que tenía como objetivo buscar, filtrar, ordenar y mostrar países, y además crear actividades
+              relacionadas con ellos,que también cumplan con el mismo requisito de buscar, filtrar y ordenar.
+              Los datos se obtenían desde una Api y se alojaban en la DB, a su vez esta era conectada tanto con el
+              back como con el front</p>
+            <footer className={style.footer}>
+              <i class="fa-solid fa-images" style={{ fontSize: "3.5vh", color: "#707070", cursor: "pointer" }}></i>
+
+              <i onClick={() => handler()} class="fa-brands fa-youtube" style={{ fontSize: "4vh", color: "#707070", cursor: "pointer", marginLeft: "3vh" }}></i>
+            </footer>
+          </aside>
           <iframe
-            width="560"
-            height="315"
+            style={{ display: `${youTube ? "flex" : "none"}` }}
+            className={style.youtube}
             src={`https://www.youtube.com/embed/${videoCode}`}
             title="YouTube Video"
             frameBorder="0"
             allowFullScreen
           ></iframe>
-        </div> */}
-        <section className={style.countries}>
-          <div className={style.imgCountries}></div>
-          <aside className={style.texto} style={{display:"flex", flexDirection:"column"}}>
-            <h1>Countries App</h1>
-            <p>App que tenía como objetivo buscar, filtrar, ordenar y mostrar países, y además crear actividades
-              relacionadas con ellos,que también cumplan con el mismo requisito de buscar, filtrar y ordenar.
-              Los datos se obtenían desde una Api y se alojaban en la DB, a su vez esta era conectada tanto con el
-              back como con el front</p>
-          </aside>
+          {/* <section className={style.galeria}>
+            <button onClick={() => { handlerChange(-1) }}><i class="fa-solid fa-caret-left"></i></button>
+            {(galeria === 0) && <img src="https://res.cloudinary.com/nahuesantini/image/upload/v1703102270/Proyectos/WhatsApp_Image_2023-11-03_at_18.18.44_tvokuo.jpg" alt="login" width={750} />}
+            {(galeria === 1) && <img src="https://res.cloudinary.com/nahuesantini/image/upload/v1703102270/Proyectos/Registro_xt9ybe.jpg" alt="registro" width={750} />}
+            {(galeria === 2) && <img src="https://res.cloudinary.com/nahuesantini/image/upload/v1703102270/Proyectos/home_ibpowk.jpg" alt="home" width={750} />}
+            {(galeria === 3) && <img src="https://res.cloudinary.com/nahuesantini/image/upload/v1703102270/Proyectos/Subir_Actividad_mlwsbp.jpg" alt="postActividad" width={750} />}
+            {(galeria === 4) && <img src="https://res.cloudinary.com/nahuesantini/image/upload/v1703102270/Proyectos/Ver_actividades_xdjnfa.jpg" alt="actividades" width={750} />}
+            <button onClick={() => handlerChange(1)}><i class="fa-solid fa-caret-right"></i></button>
+          </section> */}
+          <button style={{ display: `${youTube ? "flex" : "none"}` }} className={style.close} onClick={() => handler()}>x</button>
         </section>
         <section className={style.codecraft}>
           <aside className={style.texto}>
