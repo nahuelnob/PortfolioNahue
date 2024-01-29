3ffc86e16ea85fe40793f2bb2b7447bbb054f8da
Nahue <nahue.santini@gmail.com>
2024-01-29 17:45:05 -0300

    optimizacion en proyectos

--- src/Components/Proyectos/Proyectos.jsx
+++ src/Components/Proyectos/Proyectos.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import style from "./Proyectos.module.css";
 import Galeria from "../Galeria/Galeria";
 import YouTube from "../YouTube/YouTube";
-import datos from "../../../utils/datos.json"
+import datos from "../../utils/datos.json"
 
 const Proyectos = () => {
   const [youTube, setYoutube] = useState(false)
@@ -10,6 +10,8 @@ const Proyectos = () => {
   const handler = () => {
     setYoutube(!youTube)
   }
+
+  const proyectos = datos.Proyectos
   ////////////////////////////////////////////////////
   return (
     <div className={style.container} id="Proyectos">
@@ -20,26 +22,30 @@ const Proyectos = () => {
 
 
       <main className={style.main}>
-        <section className={style.containerMain}>
-          <div className={style.imgs}>
-            <section style={{ display: `${youTube ? "flex" : "none"}`}} className={style.youtube} >
-              <YouTube videoUrl={datos.Countries.video} />
+        {proyectos.map((datos) => {
+          const { side, nombre, imagenes, video, descripcion } = datos
+          return (
+            <section className={style.containerMain}>
+              <div className={side === "left" ? style.imgs : style.imgsRigth}>
+                <section style={{ display: `${youTube ? "flex" : "none"}` }} className={style.youtube} >
+                  <YouTube videoUrl={video} />
+                </section>
+                <section className={style.galeria}>
+                  <Galeria imagenes={imagenes} />
+                </section>
+              </div>
+              <aside className={side === "left" ? style.aside : style.asideRigth}>
+                <p className={style.parrafo}>{descripcion}</p>
+                {/* <div className={style.separadorTexto}></div> */}
+                <h1 className={style.subtitulo}>{nombre}</h1>
+                <footer className={style.footer}>
+                  <i onClick={() => handler()} class="fa-brands fa-youtube" style={{ fontSize: "4vh", color: "#707070", cursor: "pointer" }}></i>
+                </footer>
+              </aside>
+              {/* <button style={{ display: `${youTube ? "flex" : "none"}` }} className={style.close} onClick={() => handler()}><i class="fa-solid fa-rectangle-xmark" /></button> */}
             </section>
-            <section className={style.galeria}>
-              <Galeria imagenes={datos.Countries.imagenes} />
-            </section>
-          </div>
-          <aside className={style.aside} >
-            <p className={style.parrafo}>{datos.Countries.descripcion}</p>
-            {/* <div className={style.separadorTexto}></div> */}
-            <h1 className={style.subtitulo}>Countries App</h1>
-            <footer className={style.footer}>
-              <i onClick={() => handler()} class="fa-brands fa-youtube" style={{ fontSize: "4vh", color: "#707070", cursor: "pointer" }}></i>
-            </footer>
-          </aside>
-          {/* <button style={{ display: `${youTube ? "flex" : "none"}` }} className={style.close} onClick={() => handler()}><i class="fa-solid fa-rectangle-xmark" /></button> */}
-        </section>
-
+          )
+        })}
       </main>
     </div>
   );
