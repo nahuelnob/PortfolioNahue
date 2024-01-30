2a62e1bca3a9ccc70d6623104d49387646ce53ea
Nahue <nahue.santini@gmail.com>
2024-01-29 22:36:50 -0300

    Casi Finalizado

--- src/Components/Proyectos/Proyectos.jsx
+++ src/Components/Proyectos/Proyectos.jsx
@@ -5,10 +5,15 @@ import YouTube from "../YouTube/YouTube";
 import datos from "../../utils/datos.json"
 
 const Proyectos = () => {
-  const [youTube, setYoutube] = useState(false)
+  const [youTube, setYoutube] = useState({
+    "Rick & Morty" : false,
+    "Countries" : false,
+    "Codecrafted Templates" : false,
+    "Padi" : false,
+  })
 
-  const handler = () => {
-    setYoutube(!youTube)
+  const handler = (nombre) => {
+    setYoutube({[nombre] : true})
   }
 
   const proyectos = datos.Proyectos
@@ -17,7 +22,11 @@ const Proyectos = () => {
     <div className={style.container} id="Proyectos">
       <div className={style.separador}></div>
       <header className={style.header}>
-        <h1 className={style.titulo}>Proyectos</h1>
+        <h1 className={style.titulo}><span className={style.barra}>|</span>Proyectos</h1>
+        <div className={style.decorar}></div>
+        <div className={style.decorar2}></div>
+        <div className={style.decorar3}></div>
+        <div className={style.decorar4}></div>
       </header>
 
 
@@ -27,7 +36,7 @@ const Proyectos = () => {
           return (
             <section className={style.containerMain}>
               <div className={side === "left" ? style.imgs : style.imgsRigth}>
-                <section style={{ display: `${youTube ? "flex" : "none"}` }} className={style.youtube} >
+                <section style={{ display: `${youTube[`${nombre}`] ? "flex" : "none"}` }} className={style.youtube} >
                   <YouTube videoUrl={video} />
                 </section>
                 <section className={style.galeria}>
@@ -36,10 +45,10 @@ const Proyectos = () => {
               </div>
               <aside className={side === "left" ? style.aside : style.asideRigth}>
                 <p className={style.parrafo}>{descripcion}</p>
-                {/* <div className={style.separadorTexto}></div> */}
+                <div className={side === "left" ? style.separadorTexto : style.separadorTextoR}></div>
                 <h1 className={style.subtitulo}>{nombre}</h1>
                 <footer className={style.footer}>
-                  <i onClick={() => handler()} class="fa-brands fa-youtube" style={{ fontSize: "4vh", color: "#707070", cursor: "pointer" }}></i>
+                  <i onClick={() => handler(nombre)} class="fa-brands fa-youtube" style={{ fontSize: "4vh", color: "#707070", cursor: "pointer" }}></i>
                 </footer>
               </aside>
               {/* <button style={{ display: `${youTube ? "flex" : "none"}` }} className={style.close} onClick={() => handler()}><i class="fa-solid fa-rectangle-xmark" /></button> */}
